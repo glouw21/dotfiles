f6eadcd0b1ea2e999467752fb0ac13bb269cd8d1
glouw21 <lukas.vonhoeven@hk-c.de>
2026-05-26 16:09:20 +0200

    nvim configurations, added url to newsboat

---
+++ private_dot_config/nvim/options.lua
@@ -0,0 +1,46 @@
+-- Line numbers
+vim.opt.number = true
+
+-- Mouse
+vim.opt.mouse = "a"
+
+-- Encoding
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+-- No swap / backups
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
+
+-- Tabs / indentation
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- UI
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 7
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Splits
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Files
+vim.opt.fileformat = "unix"
+vim.opt.undofile = true
